78b585df780d9c4b2f616ace33263abb5e5d8fa4
MuhammadAliPeerzada <alijeejee42@gmail.com>
2022-11-29 22:14:03 +0500

    first commit

---
+++ index (1).js
@@ -0,0 +1,23 @@
+var signIn = document.getElementById("section1")
+
+var signUp = document.getElementById("section2")
+
+function showSignUp() {
+    if (signIn.style.display = "block") {
+        signUp.style.display = "block"
+        signIn.style.display = "none"
+    } 
+    
+}
+function showSignIn() {
+    if (signUp.style.display = "block") {
+        signIn.style.display = "block"
+        signUp.style.display = "none"
+    } 
+    
+}
+
+
+
+
+
